5684c1b5454c2faa65a7bceb6d81c4c321c754e2
jmohan8 <jmscit11@gmail.com>
2020-04-22 07:28:10 +0530

    test

--- src/LambdaFunction1.kt
+++ src/LambdaFunction1.kt
@@ -1,12 +1,11 @@
 fun main(Args: Array<String>) {
-    var obj_myclass = mycls()
-    var mylamda : (Int, Int) -> Unit = {a:Int ,b:Int -> println("The value of " + (a + b)) }
-    obj_myclass.sum(4,8,mylamda)
+    val obj_myclass = mycls()
+    val mylamda: (Int, Int) -> Unit = { a: Int, b: Int -> println("The value of " + (a + b)) }
+    obj_myclass.sum(4, 8, mylamda)
 }
 
-private class mycls
-{
-    fun sum(x:Int, y:Int, action: (Int,Int)->Unit){
-    action(x,y)
+private class mycls {
+    fun sum(x: Int, y: Int, action: (Int, Int) -> Unit) {
+        action(x, y)
     }
 }
--- src/LambdaWithReturn.kt
+++ src/LambdaWithReturn.kt
@@ -1,5 +1,5 @@
 fun main(Args: Array<String>) {
-    var obj_myclass = myclas()
+    val obj_myclass = myclas()
 //    var mylamda : (Int, Int) -> Int = {a:Int ,b:Int -> println("Inside lamda")
 //                                                                a + b }
 //    obj_myclass.sum(4,8,mylamda)
@@ -7,14 +7,14 @@ fun main(Args: Array<String>) {
 //    obj_myclass.sum(4,8) { a:Int, b:Int -> println("Inside lamda")
 //        a + b }
 //            Alternate ways to use lambda functions
-    obj_myclass.sum(4,8 , { a, b ->a + b })     //Both are correct
-    obj_myclass.sum(4,8) { a, b ->a + b }       //Both are corret
+    obj_myclass.sum(4, 8, { a, b -> a + b })     //Both are correct
+    obj_myclass.sum(4, 8) { a, b -> a + b }       //Both are corret
 }
-private class myclas
-{
-    fun sum(x:Int, y:Int, mylamvar: (Int,Int)->Int){
+
+private class myclas {
+    fun sum(x: Int, y: Int, mylamvar: (Int, Int) -> Int) {
 //        var result = mylamvar(x,y)
 //                  Simplyfy the above code
-        println("the value of total is " + mylamvar(x,y))
+        println("the value of total is " + mylamvar(x, y))
     }
 }
